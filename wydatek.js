9805e3127e999e70d5a21bad78dddfa61158b080
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2020-03-26 14:16:09 +0100

    Set current date on the page

--- wydatek.js
+++ wydatek.js
@@ -1,9 +1,27 @@
-function ustawDate()
+window.onload = function() 
 {
-	var dzisiaj = new Date();
-	var dzien = dzisiaj.getDate();
-	var miesiac = dzisiaj.getMonth()+1;
-	var rok = dzisiaj.getFullYear();
+	setCurrentDate();
+}
 
-	var poprawnyFormatDaty = rok + "-" + miesiac + "-" + dzien;
+function setCurrentDate()
+{
+	var dateOnPage = document.getElementById("data");
+	var today = new Date();
+
+	var day = today.getDate().toString();
+	if(day.length === 1)
+	{
+		day = '0' + day;
+	}
+	
+	var month = today.getMonth() + 1;
+	month = month.toString();
+	if(month.length === 1)
+	{
+		month = '0' + month;
+	}
+	
+	var year = today.getFullYear();
+
+	dateOnPage.value = year + "-" + month + "-" + day ;
 }
