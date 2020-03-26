3e212160388dd1a9360c552ffa1522ab7f61c5f3
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2020-03-26 14:44:35 +0100

    refactoring of setCurrentDate function

--- wydatek.js
+++ wydatek.js
@@ -9,17 +9,15 @@ function setCurrentDate()
 	var today = new Date();
 
 	var day = today.getDate().toString();
-	if(day.length === 1)
+	day = (day.length === 1) ? '0' + day : day;
+	
+	/*if(day.length === 1)
 	{
 		day = '0' + day;
-	}
+	}*/
 	
 	var month = today.getMonth() + 1;
-	month = month.toString();
-	if(month.length === 1)
-	{
-		month = '0' + month;
-	}
+	month = (month.toString().length === 1) ? '0' + month : month;
 	
 	var year = today.getFullYear();
 
