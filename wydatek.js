b398460501075811ef2bc9806b8869a4ca459d99
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2020-03-31 16:39:49 +0200

    Small changes

--- wydatek.js
+++ wydatek.js
@@ -11,15 +11,12 @@ function setCurrentDate()
 	var day = today.getDate().toString();
 	day = (day.length === 1) ? '0' + day : day;
 	
-	/*if(day.length === 1)
-	{
-		day = '0' + day;
-	}*/
-	
 	var month = today.getMonth() + 1;
 	month = (month.toString().length === 1) ? '0' + month : month;
 	
 	var year = today.getFullYear();
 
 	dateOnPage.value = year + "-" + month + "-" + day ;
+
+	console.log(dateOnPage.value)
 }
