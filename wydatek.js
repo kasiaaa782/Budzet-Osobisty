9d5b1a7419429e3279de9028a67d6a1b575c5b54
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2020-04-08 16:34:31 +0200

    Added PieChart

--- wydatek.js
+++ wydatek.js
@@ -20,3 +20,39 @@ function setCurrentDate()
 
 	console.log(dateOnPage.value)
 }
+
+//Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+	['Kategoria', 'Kwota'],
+	['Jedzenie', 8],
+	['Mieszkanie', 2],
+	['Transport', 4],
+	['Telekomunikacja', 2],
+	['Opieka zdrowotna', 8],
+	['Ubrania', 8],
+	['Higiena', 8],
+	['Dzieci', 8],
+	['Rozrywka', 8],
+	['Wycieczka', 8],
+	['Książka', 8],
+	['Oszczędności', 8],
+	['Na emeryturę', 8],
+	['Spłata długów', 8],
+	['Darowizna', 8],
+	['Inne wydatki', 8]
+	]);
+	// Optional; add a title and set the width and height of the chart
+	var options = {
+		'title':'Wykres finansowy',
+		is3D : true
+		};
+	//Display the chart inside the <div> element with id="piechart"
+	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+	chart.draw(data, options);
+}
+
